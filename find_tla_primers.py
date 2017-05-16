860f3436bc430340ff76476648d83cc661896160
allgenesconsidered <allgenesconsidered@gmail.com>
2017-05-16 09:20:09 -0700

    First commit

---
+++ find_tla_primers.py
@@ -0,0 +1,168 @@
+from Bio import SeqIO
+import primer3
+
+
+class SingleNucleotidePolymorphism(object):
+	"""
+	A class to hold SNP information.
+	"""
+	def __init__(self, name, loc, maf):
+		self.name = name
+		self.loc = loc
+		self.maf = maf
+	def __str__(self):
+		return "%s at %i with a MAF  of %f" %(self.name, self.loc, self.maf) 
+
+class TLAfragment(object):
+	"""
+	A class to hold fragment infomration, SNP placement, primer location, and 
+	primer scores for TLA.
+	"""
+	def __init__(self, seq, loc):
+		self.seq = seq
+		self.gc = self.getGC(self.seq)
+		self.start = loc[0]
+		self.end = loc[1]
+		self.snps = []
+		self.primers = {}
+		self.score = []
+	def __len__(self):
+		return len(self.seq)
+	def __str__(self):
+		string = 'index: %s\t' %str((self.start,self.end)) + 'length: %i\t' %len(self.seq) + 'gc: %i\t' %self.gc + 'snps: %i\n' %len(self.snps)
+		if len(self.seq) > 20:
+			string += self.seq[:10] + ".." + self.seq[-10:]
+		else: string += self.seq 
+		return string
+	def getGC(self, seq):
+		return float((seq.count("G") + seq.count("C")))/len(seq) * 100.0 
+	def addSNPs(self, snp):
+		if not isinstance(snp, SingleNucleotidePolymorphism):
+			raise('Error: Object not of class SingleNucleotidePolymorphism.')
+		self.snps.append(snp)
+	def addPrimers(self, primers):
+		self.primers = primers
+
+
+def basicDigest(seq, re_site,cut_index):
+	"""
+	Digests a DNA (as a string), returning TLAFragment objects that are digested
+	along with coordination information relative to the initial DNA string.  
+	"""
+	fragments = []
+	last_i = 0
+	while True:
+		i = seq.find(re_site)
+		if i == -1: # Tail end of sequence
+			fragments.append(TLAfragment(seq, (last_i, last_i+len(seq)))) 
+			return fragments
+		end = i+cut_index
+		fragments.append(TLAfragment(seq[:end],(last_i, last_i+end)))
+		seq = seq[i+cut_index:]
+		last_i += end
+
+def extractSNPfromGB(genbank_file):
+	"""
+	Extract SNP data from a genbank object, per the 'parse_genbank.py' specification. 
+	The assumption being made is that snps are labeled as 'snp' in the genbank file, 
+	and can be accessed as such. Also assumes labels ar in a "name_maf" format, per
+	the 'parse_genbank.py' file.
+
+	TODO: Add another funciton to handle SNP data straign from the XML 
+	to return SNP objects.
+	"""
+	snp_list = [(feature.qualifiers['label'][0].split('_'),int(feature.location.start)) for feature in genbank_file.features if feature.type == 'snp']
+	return [SingleNucleotidePolymorphism(snp[0][0],snp[1],float(snp[0][1])) for snp in snp_list]
+
+
+def asignSNPS(list_of_fragmets, list_of_snps):
+	"""
+	Assigns snps from a list to their coorisponding genomic location on TLA fragments
+	"""
+	for snp in list_of_snps:
+		for fragment in list_of_fragmets:
+			if fragment.start <= snp.loc < fragment.end:
+				fragment.addSNPs(snp)
+				break
+	return list_of_fragmets
+
+def subsetTLA(list_of_fragments, min_length=180, max_length=1000, min_snps=1,
+ min_gc=30, max_gc=70, flank_size=50):
+	"""
+	With SNP information added, subset a list of TLA fragments by cutoffs desirable for
+	TLA. By default, a minimum length of 180, at least 1 SNP within the first or last 50
+	nucleotides, and a %GC between 30 and 70. 
+
+	This is the function that could use the most work, at it would be preferable to return 
+	rankings or scores for each fragment, rather than arbitraty cutoffs. 
+
+	TODO: Check for excessive k-mers or repetitive sequences ('TAATAATAA').
+	"""
+	filtered_fragments = []
+	for frag in list_of_fragments:
+		if min_length <= len(frag) <= max_length and len(frag.snps) >= min_snps \
+		and min_gc <= frag.gc <= max_gc:
+			for snp in frag.snps:
+				if frag.start <= snp.loc <= (frag.start + flank_size) \
+				or (frag.end - flank_size) <= snp.loc <= frag.end:
+					filtered_fragments.append(frag)
+	return filtered_fragments
+
+def addPrimers(list_of_fragments, flank_size=50):
+	# (Idealy) define primers 
+	for frag in list_of_fragments:
+		clipped = frag.seq[flank_size:-flank_size]
+		l = len(clipped)
+		frag.addPrimers(primer3.bindings.designPrimers(
+	{
+		'SEQUENCE_ID': 'MH1000',
+		'SEQUENCE_TEMPLATE': clipped
+	},
+	{
+		'PRIMER_OPT_SIZE': 20,
+		'PRIMER_PICK_INTERNAL_OLIGO': 1,
+		'PRIMER_INTERNAL_MAX_SELF_END': 8,
+		'PRIMER_MIN_SIZE': 18,
+		'PRIMER_MAX_SIZE': 22,
+		'PRIMER_OPT_TM': 60.0,
+		'PRIMER_MIN_TM': 57.0,
+		'PRIMER_MAX_TM': 63.0,
+		'PRIMER_MIN_GC': 20.0,
+		'PRIMER_MAX_GC': 80.0,
+		'PRIMER_MAX_POLY_X': 4,
+		'PRIMER_PRODUCT_SIZE_RANGE': [[l*.2,l*.8],[l*.3,l*.7]],
+	}))
+	return list_of_fragments
+
+def handleArgs():
+	"""
+	Handles taking in command line arguments. 
+	"""
+	parser = argparse.ArgumentParser(description='Identifies optimal primers for TLA.')
+	parser.add_argument('genbank', type=str,
+                    help='A genbank file (.gb).')
+	return parser.parse_args()
+
+def main():
+	args = handleArgs()
+	genome = SeqIO.read(args.genbank,'genbank')
+	dna = str(genome.seq)
+
+	#	   0000000000111111111122222
+	#	   0123456789012345678901234
+	#dna = 'AAAAACATGTTTTCATGCCCATGAA'
+
+	tla_fragments = basicDigest(dna,'CATG',4)
+
+	snp_list = extractSNPfromGB(genome)
+	tla_fragments = asignSNPS(tla_fragments, snp_list)
+
+	tla_fragments = subsetTLA(tla_fragments)
+	
+	# Not working as expected. 
+	#tla_fragments = addPrimers(tla_fragments)
+
+
+if __name__ == '__main__':
+	main()
+
---
+++ parse_genbank.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+
+# Written by Michael Olvera 2017
+# parse_genbank.py takes in a SNP file from NCBI and a genbank file from NCBI and
+# joins the two, allowing the new ganbank file to be laded into SnapGene with SNP
+# locations annotated. 
+#
+# ./parse_genbank.py GENE_SEQUENCE.gp SNP_LIST.xml -o output_file
+#
+# XML snp files can be aquired from https://www.ncbi.nlm.nih.gov/variation/view/.
+# Genbank files can be downloaded from https://www.ncbi.nlm.nih.gov/gene/, after 
+# selecting the 'GenBank' sequence option.
+
+from Bio import SeqIO
+from Bio.SeqFeature import FeatureLocation, SeqFeature, ExactPosition
+import xml.etree.ElementTree as ET
+import re, os, argparse, sys
+
+
+def getChrStartStop(genbank_object):
+	""" 
+	Gets the start and stop on the chromosome from a genbank file, assuming its in the
+	"accessions" node.
+
+	Most genbank files have a header that starts as below:
+
+	LOCUS       NC_000010              31464 bp    DNA     linear   CON 06-JUN-2016
+	DEFINITION  Homo sapiens chromosome 10, GRCh38.p7 Primary Assembly.
+	ACCESSION   NC_000010 REGION: 119648701..119680164 GPC_000001302
+
+	Where '119648701..119680164' is the chromosomal position of the genbank. This function should
+	reliably extract that file.
+	"""
+	accession = genbank_object.annotations["accessions"]
+	non_decimal = re.compile(r'[^\d.]+')
+	# Within the Genbank File, grab the chromosomal locations from  
+	g_range=[non_decimal.sub('',i) for i in accession if re.search('[0-9]+\.\.[0-9]+', i)] # genomic location
+	start,end = g_range[0].split('..')
+	return int(start), int(end)
+
+def parseXMLforSNPs(xml_file):
+	"""
+	Parses the xml file containing SNPs and outputs a list containing tuples of the name of the SNP,
+	the genomic location, and the MAF. 
+
+	TODO Include a cutoff option for minimum MAF value. 
+	"""
+	tree = ET.parse(xml_file).getroot()
+	snp_list = []
+	for snp in tree.findall('./Variants/Variant'):
+		snp_list.append( (snp.find('VariantID').text, \
+			int(snp.find('Location').attrib['start']), float(snp.find('MinorAlleleFrequencies/MAF').text)))
+	return snp_list
+
+def addSNPFeatures(genbank, snp_list):
+	"""
+	Function to add SNP lists to the genbank file. The only qualifier sofar is the 'name', which
+	merges the SNP name with the MAF for visibility on SnapGene.
+
+	There is an unfortunate bug in SnapGene where 1bp long features are automatically converted to
+	2 base pairs. I will contact Snapgene to try to get the issue resolved, although know that
+	the frount of the feature is its location.
+	"""
+	for snp in snp_list:
+		l = ExactPosition(snp[1])
+		location = FeatureLocation(l,l)
+		name = snp[0] + '_' + '%.3f' %snp[2]
+		snp_feature = SeqFeature(location,type='snp', id=name, qualifiers={'label':name})
+		genbank.features.append(snp_feature)
+	return genbank
+
+def trimSNPs(snp_list, start, end):
+	"""
+	Trims the length of the SNP locations to reflect the location in the abridged genbank file.
+	By default, SMP locations are based on chromosomal coordinates, while genbank is relative. 
+	This will convert all SNPs to their proper locations while also removing SNPs outside the 
+	bounds of the genbank file. 
+	"""
+	length = end - start
+	name, loc, freq = [sep for sep in zip(*snp_list)]
+	loc = tuple(snp - start for snp in loc)
+	oor_indexes = oorSNPs(loc,length)
+
+	filtered_snps = [(name[i],loc[i],freq[i]) for i, _ in enumerate(name) if i not in oor_indexes]
+	return filtered_snps
+
+
+def oorSNPs(snp_len_list, genbank_len):
+	"""
+	Works with trimSNPs() to remove SNP out of bounds from the genbank file. Outputs the number
+	of SNPs excluded, and information about how to extend the genbank file if you wish to include
+	the SNPs in the future. 
+	"""
+	oor_snps_minus = [(i, snp) for i, snp in enumerate(snp_len_list) if snp < 0]
+	oor_spns_plus = [(i, snp) for i, snp in enumerate(snp_len_list) if snp > genbank_len]
+	oor_snps = oor_spns_plus + oor_snps_minus
+	if not oor_snps:
+		return []
+
+	index, loc = [i for i in zip(*oor_snps)]
+	errorM = "\nWarning: %i SNPs out of range " %len(index) 
+	if not oor_snps_minus:
+		errorM += "(0, "
+	else: errorM += "(%i, " %min(loc)
+	if not oor_spns_plus:
+		errorM += "+0)."
+	else: errorM += "+%i).\n" %(max(loc)-genbank_len)
+
+	print(errorM)
+	return index
+		
+def checkAssembly(genbank, xml):
+	"""
+	Make sure the genome assembly files are the same. For the genbnak files, its
+	usually burried in the comment section:
+
+	COMMENT     REFSEQ INFORMATION: The reference sequence is identical to
+            CM000672.2.
+            On Feb 3, 2014 this sequence version replaced gi:224589801.
+            Assembly Name: GRCh38.p7 Primary Assembly
+            The DNA sequence is composed of genomic sequence, primarily
+            finished clones that were sequenced as part of the Human Genome
+            Project. PCR products and WGS shotgun sequence have been added
+            where necessary to fill gaps or correct errors. All such additions
+            are manually curated by GRC staff. For more information see:
+            http://genomereference.org.
+
+    Wherease for the XML is in the VariationData/MetaData/AssemblyName node:
+
+  <VariationData>
+  	<Metadata>
+	    <TaxID>9606</TaxID>
+	    <OrganismScientificName>Homo sapiens</OrganismScientificName>
+	    <AssemblyName>GRCh38.p7</AssemblyName>
+	"""
+	tree = ET.parse(xml).getroot()
+	version = tree.find('./Metadata/AssemblyName').text
+
+	return version in genbank.annotations['comment'] # True if assemblies are the same
+
+def handleArgs():
+	"""
+	Handles taking in command line arguments. 
+	"""
+	parser = argparse.ArgumentParser(description='Take in a raw genbank file and adds SNP data. Output can be used in Snapgene.')
+	parser.add_argument('genbank', type=str,
+                    help='A genbank file (.gb).')
+	parser.add_argument('snps_input',  type=str,
+                    help='A list of SNPs in XML format.')
+	parser.add_argument('-o','--output', type=str,
+                    help='File name to output, without the .gb.')
+	return parser.parse_args()
+
+def main():
+
+	args = handleArgs()
+	genome = SeqIO.read(args.genbank,'genbank') # You MUST tell SeqIO what format is being read
+	# Run a check to make sure assemblies are matching.
+	if not checkAssembly(genome, args.snps_input):
+		raise StandardError('Genome assembly files are not the same between SNP list and genbank file.')
+	# Get start and end points. 
+	start, end = getChrStartStop(genome)
+	snps = parseXMLforSNPs(args.snps_input)
+	# Check for SNP outside the range of the genbank file.
+	snps = trimSNPs(snps, start, end)
+	# Add new SNP features 
+	genome = addSNPFeatures(genome, snps)
+	with open(args.output + '.gb', 'w') as out:
+		SeqIO.write(genome, out,'genbank')
+
+if __name__ == '__main__':
+	main()
+
+
+
+
